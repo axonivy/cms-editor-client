38ab0ec4e613149a454d5965f3616498d1409a8d
Lukas Giger <lukas.giger@axonivy.com>
2025-03-27 14:30:38 +0100

    XIVY-15943 always have default language

--- packages/cms-editor/src/utils/use-client-language.ts
+++
@@ -1,8 +0,0 @@
-import i18next from 'i18next';
-import { useMemo } from 'react';
-
-export const useClientLanguage = () => {
-  const clientLanguageTag = i18next.language;
-  const languageDisplayName = useMemo(() => new Intl.DisplayNames([clientLanguageTag], { type: 'language' }), [clientLanguageTag]);
-  return { clientLanguageTag, languageDisplayName };
-};
