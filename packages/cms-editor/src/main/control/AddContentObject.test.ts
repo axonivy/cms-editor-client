e6a29a167cb89aa63d6a52c26906f30355f1b8c7
Lukas Giger <lukas.giger@axonivy.com>
2025-04-02 15:02:08 +0200

    XIVY-15943 add options to namespace

--- packages/cms-editor/src/main/control/AddContentObject.test.ts
+++ packages/cms-editor/src/main/control/AddContentObject.test.ts
@@ -1,9 +1,23 @@
 import type { ContentObject } from '@axonivy/cms-editor-protocol';
-import { initialNamespace } from './AddContentObject';
+import { initialNamespace, namespaceOptions } from './AddContentObject';
 
 test('initialNamespace', () => {
   const contentObjects = [{ uri: '/contentObject' }, { uri: '/folder/deep/contentObject' }] as Array<ContentObject>;
   expect(initialNamespace(contentObjects, undefined)).toEqual('');
   expect(initialNamespace(contentObjects, 0)).toEqual('');
   expect(initialNamespace(contentObjects, 1)).toEqual('/folder/deep');
+});
+
+test('namespaceOptions', () => {
+  expect(
+    namespaceOptions([
+      { uri: '/a' },
+      { uri: '/b' },
+      { uri: '/c/a' },
+      { uri: '/c/b' },
+      { uri: '/c/c/a' },
+      { uri: '/c/c/b' },
+      { uri: '/d/a' }
+    ] as Array<ContentObject>)
+  ).toEqual([{ value: '' }, { value: '/c' }, { value: '/c/c' }, { value: '/d' }]);
 });
