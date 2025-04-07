0727afd18eb36f455ebaddfba8528298e23f8be6
Lukas Giger <lukas.giger@axonivy.com>
2025-04-07 07:03:38 +0200

    XIVY-15943 update values on change

--- packages/cms-editor/src/detail/DetailContent.tsx
+++ packages/cms-editor/src/detail/DetailContent.tsx
@@ -1,5 +1,5 @@
-import type { CmsData, CmsDeleteValueArgs, CmsDeleteValueObject, ContentObject } from '@axonivy/cms-editor-protocol';
-import { BasicField, BasicInput, Flex, PanelMessage, Spinner } from '@axonivy/ui-components';
+import type { CmsData, CmsDeleteValueArgs, CmsUpdateValueArgs, ContentObject, MapStringString } from '@axonivy/cms-editor-protocol';
+import { BasicField, BasicInput, Flex, PanelMessage, Spinner, type Unary } from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useCallback } from 'react';
@@ -22,33 +22,45 @@ export const DetailContent = () => {
 
   const uri = selectedContentObject !== undefined ? contentObjects[selectedContentObject].uri : '';
 
-  const removeValueFromReadQuery = useCallback(
-    (deleteObject: CmsDeleteValueObject) =>
-      queryClient.setQueryData<ContentObject>(readKey({ context, uri: deleteObject.uri }), data => {
+  const updateValuesInReadQuery = useCallback(
+    (uri: string, valueUpdater: Unary<MapStringString>) =>
+      queryClient.setQueryData<ContentObject>(readKey({ context, uri }), data => {
         if (!data) {
           return;
         }
-        return { ...data, values: removeValue(data.values, deleteObject.languageTag) };
+        return { ...data, values: valueUpdater(data.values) };
       }),
     [context, queryClient, readKey]
   );
 
-  const removeValueFromDataQuery = useCallback(
-    (deleteObject: CmsDeleteValueObject) =>
+  const updateValuesInDataQuery = useCallback(
+    (uri: string, valueUpdater: Unary<MapStringString>) =>
       queryClient.setQueryData<CmsData>(dataKey({ context, languageTags: [defaultLanguageTag] }), data => {
         if (!data) {
           return;
         }
-        return removeValueFromContentObjectInData(data, deleteObject.uri, deleteObject.languageTag);
+        return updateValuesOfContentObjectInData(data, uri, valueUpdater);
       }),
     [context, dataKey, defaultLanguageTag, queryClient]
   );
 
+  const updateMutation = useMutation({
+    mutationFn: async (args: CmsUpdateValueArgs) => {
+      const changeValueUpdater = (values: MapStringString) => ({ ...values, [args.updateObject.languageTag]: args.updateObject.value });
+      updateValuesInReadQuery(args.updateObject.uri, changeValueUpdater);
+      if (args.updateObject.languageTag === defaultLanguageTag) {
+        updateValuesInDataQuery(args.updateObject.uri, changeValueUpdater);
+      }
+      return client.updateValue(args);
+    }
+  });
+
   const deleteMutation = useMutation({
     mutationFn: async (args: CmsDeleteValueArgs) => {
-      removeValueFromReadQuery(args.deleteObject);
+      const deleteValueUpdater = (values: MapStringString) => removeValue(values, args.deleteObject.languageTag);
+      updateValuesInReadQuery(args.deleteObject.uri, deleteValueUpdater);
       if (args.deleteObject.languageTag === defaultLanguageTag) {
-        removeValueFromDataQuery(args.deleteObject);
+        updateValuesInDataQuery(args.deleteObject.uri, deleteValueUpdater);
       }
       return client.deleteValue(args);
     }
@@ -95,7 +107,9 @@ export const DetailContent = () => {
           <CmsValueField
             key={languageTag}
             values={contentObject.values}
-            updateValue={() => {}}
+            updateValue={(languageTag: string, value: string) =>
+              updateMutation.mutate({ context, updateObject: { uri, languageTag, value } })
+            }
             deleteValue={(languageTag: string) => deleteMutation.mutate({ context, deleteObject: { uri, languageTag } })}
             languageTag={languageTag}
             disabledDelete={hasExactlyOneValue}
@@ -111,13 +125,13 @@ export const DetailContent = () => {
   );
 };
 
-export const removeValueFromContentObjectInData = (data: CmsData, uri: string, languageTag: string) => {
+export const updateValuesOfContentObjectInData = (data: CmsData, uri: string, valueUpdater: Unary<MapStringString>) => {
   const index = data.data.findIndex(co => co.uri === uri);
   if (index === -1) {
     return;
   }
   const co = data.data[index];
-  const newCo = { ...co, values: removeValue(co.values, languageTag) };
+  const newCo = { ...co, values: valueUpdater(co.values) };
   const newData = [...data.data];
   newData[index] = newCo;
   return { ...data, data: newData };
