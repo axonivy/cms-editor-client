505f2a20c5ce6c8ce15f4ed70dd3e3b54c422e31
Fabian Heuberger <fabian.heuberger@axonivy.com>
2025-04-09 10:56:31 +0200

    XIVY-16283 moved common ns into default ns prefixed by 'common'

--- packages/cms-editor/src/utils/hotkeys.ts
+++ packages/cms-editor/src/utils/hotkeys.ts
@@ -8,7 +8,7 @@ export const useKnownHotkeys = () => {
   const { t } = useTranslation();
   const openHelp = useMemo<KnownHotkey>(() => {
     const hotkey = 'F1';
-    return { hotkey, label: t('common:hotkey.help', { hotkey: hotkeyText(hotkey) }) };
+    return { hotkey, label: t('common.hotkey.help', { hotkey: hotkeyText(hotkey) }) };
   }, [t]);
 
   const addContentObject = useMemo<KnownHotkey>(() => {
@@ -23,17 +23,17 @@ export const useKnownHotkeys = () => {
 
   const focusToolbar = useMemo<KnownHotkey>(() => {
     const hotkey = '1';
-    return { hotkey, label: t('common:hotkey.focusToolbar', { hotkey: hotkeyText(hotkey) }) };
+    return { hotkey, label: t('common.hotkey.focusToolbar', { hotkey: hotkeyText(hotkey) }) };
   }, [t]);
 
   const focusMain = useMemo<KnownHotkey>(() => {
     const hotkey = '2';
-    return { hotkey, label: t('common:hotkey.focusMain', { hotkey: hotkeyText(hotkey) }) };
+    return { hotkey, label: t('common.hotkey.focusMain', { hotkey: hotkeyText(hotkey) }) };
   }, [t]);
 
   const focusInscription = useMemo<KnownHotkey>(() => {
     const hotkey = '3';
-    return { hotkey, label: t('common:hotkey.focusInscription', { hotkey: hotkeyText(hotkey) }) };
+    return { hotkey, label: t('common.hotkey.focusInscription', { hotkey: hotkeyText(hotkey) }) };
   }, [t]);
 
   return { openHelp, addContentObject, deleteContentObject, focusToolbar, focusMain, focusInscription };
