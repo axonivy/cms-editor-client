104054b980d4c9e8a59fb4cc1f49cd9ee00669f9
Lukas Giger <lukas.giger@axonivy.com>
2025-05-05 16:17:38 +0200

    XIVY-15945 wrap columns in `useMemo`

--- packages/cms-editor/src/main/MainContent.tsx
+++ packages/cms-editor/src/main/MainContent.tsx
@@ -20,7 +20,7 @@ import {
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { flexRender, getCoreRowModel, useReactTable, type ColumnDef, type Row } from '@tanstack/react-table';
 import { useVirtualizer } from '@tanstack/react-virtual';
-import { useRef } from 'react';
+import { useMemo, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAppContext } from '../context/AppContext';
 import { useClient } from '../protocol/ClientContextProvider';
@@ -55,27 +55,30 @@ export const MainContent = () => {
 
   const globalFilter = useTableGlobalFilter();
 
-  const columns: Array<ColumnDef<ContentObject, string>> = [
-    {
-      accessorKey: 'uri',
-      header: ({ column }) => <SortableHeader column={column} name='URI' />,
-      cell: cell => <span>{cell.getValue()}</span>,
-      minSize: 200,
-      size: 500,
-      maxSize: 1000
-    }
-  ];
-  toLanguages(defaultLanguageTags, languageDisplayName).forEach(language =>
-    columns.push({
-      id: language.value,
-      accessorFn: co => co.values[language.value],
-      header: ({ column }) => <SortableHeader column={column} name={language.label} />,
-      cell: cell => <span>{cell.getValue()}</span>,
-      minSize: 200,
-      size: 500,
-      maxSize: 1000
-    })
-  );
+  const columns = useMemo(() => {
+    const columns: Array<ColumnDef<ContentObject, string>> = [
+      {
+        accessorKey: 'uri',
+        header: ({ column }) => <SortableHeader column={column} name='URI' />,
+        cell: cell => <span>{cell.getValue()}</span>,
+        minSize: 200,
+        size: 500,
+        maxSize: 1000
+      }
+    ];
+    toLanguages(defaultLanguageTags, languageDisplayName).forEach(language =>
+      columns.push({
+        id: language.value,
+        accessorFn: co => co.values[language.value],
+        header: ({ column }) => <SortableHeader column={column} name={language.label} />,
+        cell: cell => <span>{cell.getValue()}</span>,
+        minSize: 200,
+        size: 500,
+        maxSize: 1000
+      })
+    );
+    return columns;
+  }, [defaultLanguageTags, languageDisplayName]);
 
   const table = useReactTable({
     ...selection.options,
